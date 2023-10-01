18ea680b3a7fca17f051ff8df7834a973a755dcf
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2023-10-01 12:38:03 +0530

    Added another route

--- server/index.js
+++ server/index.js
@@ -5,11 +5,15 @@ app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("Hello from AWS!");
-})
+});
 
 app.get("/newRoute", (req, res) => {
     res.send("Welcome to the new route");
-})
+});
+
+app.get("/anotherRoute",(req, res)=>{
+res.send("Added another route!!!!");
+});
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
